01456d8a4805aa164271d249a1258f14c8137e8d
Hasu <syu8.0324@outlook.jp>
2020-02-29 03:43:45 +0000

    0が消えるバグの処理

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 let l_side = 0;
 let r_side = 0;
-let tax =0;
+let tax = 0;
 let parsed_l, parsed_r;
 let ope = [];
 let flag = 1; //1...左辺入力状態、2...演算子入力状態、3...右辺入力状態
 let result = document.getElementById('result');
 
-
-function change_other_number(){
-  let bn = document.getElementById("basenumber");
+function change_other_number() {
+  let bn = document.getElementById('basenumber');
 }
 
 function replace_str(a, b) {
@@ -16,25 +15,24 @@ function replace_str(a, b) {
 }
 //かんたんな四則演算ができるところまでとりあえず実装する。
 
-function set_value(){
-      l_side = result.value;
-      r_side = 0;
-      console.log('l_side :' + l_side, 'r_side :' + r_side, 'ope :' + ope);
+function set_value() {
+  l_side = result.value;
+  r_side = 0;
+  //console.log('l_side :' + l_side, 'r_side :' + r_side, 'ope :' + ope);
 }
-
 
 //数字入力判定関数
 function input(num) {
-  
+  if (flag == 1 && result.value == '0') replace_str(0, '');
+
   if (flag == 1) {
-    replace_str(0, '');
     l_side = result.value = result.value + num.value;
-    console.log('l_side :' + l_side);
+    //console.log('l_side :' + l_side);
   } else if (flag == 2) {
     replace_str(l_side, '');
     r_side = result.value = result.value + num.value;
     r_side.trim();
-    console.log('r_side :', r_side);
+    //console.log('r_side :', r_side);
   }
   if (num.value == 'C') clear();
   if (num.value == 'AC') all_clear();
@@ -43,19 +41,19 @@ function input(num) {
 
 function operator(num) {
   ope.unshift(num.value); //演算子記憶
-  console.log('ope_length :' + ope.length, 'ope :' + ope, 'l_side :' + l_side, 'r_side :' + r_side);
-  
-  if(num.value == '%') {
-    r_side = 100; 
+  //console.log('ope_length :' + ope.length, 'ope :' + ope, 'l_side :' + l_side, 'r_side :' + r_side);
+
+  if (num.value == '%') {
+    r_side = 100;
     calc();
   }
-  
+
   if (l_side != 0 && r_side != 0 && ope.length > 1) calc();
   flag = 2;
 }
 
 function calc() {
-  console.log(ope);
+  //console.log(ope);
 
   parsed_l = parseInt(l_side);
   parsed_r = parseInt(r_side);
@@ -77,11 +75,11 @@ function calc() {
       set_value();
       break;
     case '^':
-     result.value = parsed_l**parsed_r;
+      result.value = parsed_l ** parsed_r;
       set_value();
-     break;
+      break;
     case '%':
-      result.value = parsed_l/parsed_r;
+      result.value = parsed_l / parsed_r;
       set_value();
       break;
   }
@@ -95,14 +93,14 @@ function clear() {
   }
   flag = 1;
   result.value = 0;
-  console.log('l_side :' + l_side, 'r_side :' + r_side,'flag :'+flag);
+  //console.log('l_side :' + l_side, 'r_side :' + r_side, 'flag :' + flag);
 }
 
 function all_clear() {
   l_side = 0;
   r_side = 0;
-  ope.length =0;
+  ope.length = 0;
   flag = 1;
   result.value = 0;
-  console.log('l_side :' + l_side, 'r_side :' + r_side,'flag :'+flag);
+  //console.log('l_side :' + l_side, 'r_side :' + r_side, 'flag :' + flag);
 }
