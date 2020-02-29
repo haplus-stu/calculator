12b9c9b3d7f9344b306a8839f36c4d7dc8dc36a4
Vagrant Default User <vagrant@buster.localdomain>
2020-02-29 02:21:10 +0000

    修正

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let l_side = 0;
 let r_side = 0;
+let tax =0;
 let parsed_l, parsed_r;
 let ope = [];
 let flag = 1; //1...左辺入力状態、2...演算子入力状態、3...右辺入力状態
 let result = document.getElementById('result');
+
 
 function change_other_number(){
   let bn = document.getElementById("basenumber");
@@ -20,6 +22,7 @@ function set_value(){
       console.log('l_side :' + l_side, 'r_side :' + r_side, 'ope :' + ope);
 }
 
+
 //数字入力判定関数
 function input(num) {
   
@@ -34,12 +37,19 @@ function input(num) {
     console.log('r_side :', r_side);
   }
   if (num.value == 'C') clear();
+  if (num.value == 'AC') all_clear();
   change_other_number();
 }
 
 function operator(num) {
   ope.unshift(num.value); //演算子記憶
   console.log('ope_length :' + ope.length, 'ope :' + ope, 'l_side :' + l_side, 'r_side :' + r_side);
+  
+  if(num.value == '%') {
+    r_side = 100; 
+    calc();
+  }
+  
   if (l_side != 0 && r_side != 0 && ope.length > 1) calc();
   flag = 2;
 }
@@ -70,6 +80,10 @@ function calc() {
      result.value = parsed_l**parsed_r;
       set_value();
      break;
+    case '%':
+      result.value = parsed_l/parsed_r;
+      set_value();
+      break;
   }
 }
 
@@ -79,6 +93,15 @@ function clear() {
   } else if (l_side != 0 && r_side != 0) {
     r_side = 0;
   }
+  flag = 1;
+  result.value = 0;
+  console.log('l_side :' + l_side, 'r_side :' + r_side,'flag :'+flag);
+}
+
+function all_clear() {
+  l_side = 0;
+  r_side = 0;
+  ope.length =0;
   flag = 1;
   result.value = 0;
   console.log('l_side :' + l_side, 'r_side :' + r_side,'flag :'+flag);
